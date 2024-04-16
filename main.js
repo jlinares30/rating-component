fe0f37cd8a4e70551956671f186beb9c46434c81
jlinares30 <jorgelinaresrrs306@hotmail.com>
2024-04-16 04:42:15 -0500

    uploading project mobile first

---
+++ main.js
@@ -0,0 +1,6 @@
+let cardSubmit = document.querySelector('.card-container-submit')
+let cardThanks = document.querySelector('.card-container-thanks')
+let numbers = document.querySelectorAll('.numbers')
+let submit = document.querySelector('.submit')
+let ratingSelected
+
