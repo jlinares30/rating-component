22b0bc17e15d3c16545d172b8a031a89fc24e8ac
jlinares30 <jorgelinaresrrs306@hotmail.com>
2024-04-20 08:01:53 -0500

    add js-complete

--- main.js
+++ main.js
@@ -2,5 +2,29 @@ let cardSubmit = document.querySelector('.card-container-submit')
 let cardThanks = document.querySelector('.card-container-thanks')
 let numbers = document.querySelectorAll('.numbers')
 let submit = document.querySelector('.submit')
+let numberSelected = document.querySelector('#number-selected')
+let originalBackgroundColor = '#959eac34'
+let originalNumberColor = '#959eac'
 let ratingSelected
-
+   
+    numbers.forEach(number =>{
+        number.addEventListener('click',()=>{
+            ratingSelected = number.textContent
+            number.style.backgroundColor = '#fa7312'
+            number.style.color = 'white'
+            numbers.forEach(num =>{
+                if(num != number){
+                    num.style.backgroundColor = originalBackgroundColor
+                    num.style.color = originalNumberColor
+                }
+            })
+            console.log(ratingSelected)
+        })
+    })
+submit.addEventListener('click',()=>{
+    if(!isNaN(ratingSelected)){
+        cardSubmit.hidden = true
+        cardThanks.hidden = false
+        numberSelected.textContent = ratingSelected
+    }
+})
